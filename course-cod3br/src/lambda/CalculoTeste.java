dca38ea24e85c63d35dc19b4c0815d0f7b29a748
Bruno Lima <brunopclima81@gmail.com>
2020-05-25 00:15:49 -0300

    add more exercises

---
+++ course-cod3br/src/lambda/CalculoTeste.java
@@ -0,0 +1,16 @@
+package lambda;
+
+public class CalculoTeste {
+	
+	public static void main(String[] args) {
+		Calculo calculo = new Somar();
+		System.out.println(calculo.executar(2, 3));
+
+		calculo = new Multiplicar();
+		System.out.println(calculo.executar(2, 3));
+		
+		
+		
+	}
+
+}
---
+++ course-cod3br/src/lambda/Predicado.java
@@ -0,0 +1,18 @@
+package lambda;
+
+import java.util.function.Predicate;
+
+public class Predicado {
+
+	public static void main(String[] args) {
+		
+		Predicate<Produto> isCaro = prod -> (prod.preco * (1 - prod.desconto)) >= 750;
+				
+		Produto macBookAir = new Produto("MacBook Air", 2000.00, 0.75);
+		Produto penDrive = new Produto("Pen Drive", 50.00, 0.0);
+		
+		System.out.println("MacBook Air é caro: " + isCaro.test(macBookAir));
+		System.out.println("Pendrive é caro: " + isCaro.test(penDrive));
+	}
+	
+}
