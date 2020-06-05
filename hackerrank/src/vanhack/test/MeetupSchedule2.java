f63ea84cf25010637ba247c7b71bca1669f71821
Bruno Lima <brunopclima81@gmail.com>
2020-06-05 09:53:19 -0300

    check

--- hackerrank/src/vanhack/test/MeetupSchedule2.java
+++ hackerrank/src/vanhack/test/MeetupSchedule2.java
@@ -3,14 +3,10 @@ package vanhack.test;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.LinkedHashSet;
-import java.util.LinkedList;
+import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
-import java.util.TreeMap;
+import java.util.TreeSet;
 
 class CountMeetings2 {
 
@@ -22,129 +18,117 @@ class CountMeetings2 {
 	 */
 
 	public static int countMeetings(List<Integer> firstDay, List<Integer> lastDay) {
-        int sizeFirstDay = firstDay.size();
-        int sizeLastDay = lastDay.size();
+		int sizeFirstDay = firstDay.size();
+		int sizeLastDay = lastDay.size();
 
-        if (sizeFirstDay != sizeLastDay) {
-            return 0;
-        }
-
-        // Constraint: 1 <= n <= 100000
-        if (sizeFirstDay <= 1 && sizeFirstDay > 100000) {
-            return 0;
-        }
-
-        // Constraint: 1 <= n <= 100000
-        if (sizeLastDay <= 1 && sizeLastDay > 100000) {
-            return 0;
-        }
-
-        // Constraint: 1 <= firstDay[i], lastDay[i] <= 100000 (where 0 <= i < n)
-        // Constraint: firstDay[i] <= lastDay[i] (where 0 <= i < n)
-
-        List<Investor2> investidores = new ArrayList<Investor2>();
-        List<Investor2> agendaAux = new ArrayList<Investor2>();
-
-        for (int i = 0; i < lastDay.size(); i++) {
-            if (firstDay.get(i) <= lastDay.get(i)) {
-                investidores.add(new Investor2(firstDay.get(i), lastDay.get(i)));
-            }
-        }
-        System.out.println("QTDE Investidores: " + investidores.size());
-
-        Set<Investor2> agenda = new HashSet<Investor2>();
-
-        for (Investor2 investor : investidores) {
-            if (investor.getBeginDay() == investor.getEndDay()) {
-                agenda.add(investor);
-            } else {
-                agendaAux.add(investor);
-            } 
-        }
-        System.out.println("QTDE Investidores JAH Agendados1: " + agenda.size());
-        System.out.println("QTDE Investidores NAO Agendados1: " + agendaAux.size());
- 
-   
-        List<Investor2> agendaAux2 = new ArrayList<Investor2>(agendaAux);
-        System.out.println("QTDE Investidores agendaAux2: " + agendaAux2.size());
-        
-        
-        for (int i = 0; i < agendaAux2.size(); i++) {
-        	if (!agenda.contains(agendaAux2.get(i).getBeginDay())) {
-                agenda.add(agendaAux2.get(i));
-                agendaAux.remove(agendaAux2.get(i));
-            } else if (!agenda.contains(agendaAux2.get(i).getEndDay())) {
-                agenda.add(agendaAux2.get(i));
-                agendaAux.remove(agendaAux2.get(i));
-            } 
+		if (sizeFirstDay != sizeLastDay) {
+			return 0;
 		}
-  
-        System.out.println("QTDE Investidores JAH Agendados2: " + agenda.size());
-        System.out.println("QTDE Investidores NAO Agendados2: " + agendaAux.size());
 
-        if (!agendaAux.isEmpty()) {
-        	for (int i = 0; i < agendaAux.size(); i++) {
-            	if (!agenda.contains(agendaAux.get(i))) {
-                    agenda.add(agendaAux.get(i));
-                } else if (!agenda.contains(agendaAux.get(i))) {
-                    agenda.add(agendaAux.get(i));
-                } 
-    		}
-        }
-        
-        System.out.println("QTDE Investidores JAH Agendados3: " + agenda.size());
-        System.out.println("QTDE Investidores QUE sobraram3: " + agendaAux.size());
-        System.out.println(agenda.contains(new Investor2(62873, 88533)));
-        return agenda.size();
-    }
-	
+		// Constraint: 1 <= n <= 100000
+		if (sizeFirstDay <= 1 && sizeFirstDay > 100000) {
+			return 0;
+		}
+
+		// Constraint: 1 <= n <= 100000
+		if (sizeLastDay <= 1 && sizeLastDay > 100000) {
+			return 0;
+		}
+
+		// Constraint: 1 <= firstDay[i], lastDay[i] <= 100000 (where 0 <= i < n)
+		// Constraint: firstDay[i] <= lastDay[i] (where 0 <= i < n)
+
+		List<Investor2> lsInvestidores = new ArrayList<Investor2>();
+		for (int i = 0; i < lastDay.size(); i++) {
+			if (firstDay.get(i) <= lastDay.get(i)) {
+				lsInvestidores.add(new Investor2(firstDay.get(i), lastDay.get(i)));
+			}
+		}
+
+		for (Investor2 investor : lsInvestidores) {
+			System.out.println(investor);
+		}
+
+		Set<Integer> agenda = new TreeSet<Integer>();
+
+		List<Investor2> lsInvestidoresAux = new ArrayList<Investor2>(lsInvestidores);
+		for (Investor2 investor : lsInvestidoresAux) {
+			if (investor.getBeginDay() == investor.getEndDay()) {
+				agenda.add(investor.getBeginDay());
+				lsInvestidores.remove(investor);
+			}
+		}
+
+		System.out.println("============");
+		for (Integer diaAgendado : agenda) {
+			System.out.println(diaAgendado);
+		}
+
+		System.out.println("============");
+		for (Investor2 investor : lsInvestidores) {
+			System.out.println(investor);
+		}
+
+		for (int i = 0; i < lsInvestidores.size(); i++) {
+			Investor2 investidor = lsInvestidores.get(i);
+			
+			for (Integer diaAgendado : agenda) {
+				if (!diaAgendado.equals(investidor.getBeginDay())) {
+					agenda.add(investidor.getBeginDay());
+					lsInvestidores.remove(investidor);
+					break;
+				} else if (!diaAgendado.equals(investidor.getEndDay())) {
+					agenda.add(investidor.getEndDay());
+					lsInvestidores.remove(investidor);
+					break;
+				}
+			}
+		}
+		
+		System.out.println("============");
+		for (Integer diaAgendado : agenda) {
+			System.out.println(diaAgendado);
+		}
+		
+
+		return agenda.size();
+	}
 
 }
 
-class Investor2 {
+class Investor2 implements Comparable<Investor2>{
 
-	private int id;
-	private int beginDay;
-	private int endDay;
-
-	private static int valueId = 1;
-
+	private Integer beginDay;
+	private Integer endDay;
+	
 	public Investor2(Integer beginDay, Integer endDay) {
-		this.id = valueId;
 		this.beginDay = beginDay;
 		this.endDay = endDay;
-		valueId++;
 	}
 
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public int getBeginDay() {
+	public Integer getBeginDay() {
 		return beginDay;
 	}
 
-	public int getEndDay() {
+	public void setBeginDay(Integer beginDay) {
+		this.beginDay = beginDay;
+	}
+
+	public Integer getEndDay() {
 		return endDay;
 	}
 
-	@Override
-	public String toString() {
-//		return "Investor2 [id=" + id + ", beginDay=" + beginDay + ", endDay=" + endDay + "]";
-		return "Investor2 [beginDay=" + beginDay + ", endDay=" + endDay + "]";
+	public void setEndDay(Integer endDay) {
+		this.endDay = endDay;
 	}
 
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + beginDay;
-		result = prime * result + endDay;
-//		result = prime * result + id;
+		result = prime * result + ((beginDay == null) ? 0 : beginDay.hashCode());
+		result = prime * result + ((endDay == null) ? 0 : endDay.hashCode());
 		return result;
 	}
 
@@ -157,13 +141,36 @@ class Investor2 {
 		if (getClass() != obj.getClass())
 			return false;
 		Investor2 other = (Investor2) obj;
-		if (beginDay != other.beginDay)
+		if (beginDay == null) {
+			if (other.beginDay != null)
+				return false;
+		} else if (!beginDay.equals(other.beginDay))
 			return false;
-		if (endDay != other.endDay)
+		if (endDay == null) {
+			if (other.endDay != null)
+				return false;
+		} else if (!endDay.equals(other.endDay))
 			return false;
-//		if (id != other.id)
-//			return false;
 		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Investor2 [beginDay=" + beginDay + ", endDay=" + endDay + "]";
+	}
+
+	@Override
+	public int compareTo(Investor2 o) {
+		if (this.getBeginDay() > o.getBeginDay() && this.getEndDay() > o.getEndDay()) {
+			return 1;
+		} else if (this.getBeginDay() < o.getBeginDay() && this.getEndDay() < o.getEndDay()) {
+			return -1;
+		} else if (this.getBeginDay().equals(o.getBeginDay()) && this.getEndDay().equals(o.getEndDay())) {
+			return 0;
+		} else {
+			return 0;
+		}
+		
 	}
 
 }
